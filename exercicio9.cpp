8760e62c7f89d84d8a916f78d5ee5130e2d45054
Paulo_camara/Dev <99374349+26411988@users.noreply.github.com>
2024-03-28 00:35:54 +0000

    exercicioLogicaProgracao

---
+++ exercicio9.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+    int main () {  
+    
+    int num;
+
+printf("digite um numero:");
+scanf("%d", &num);
+
+printf("%\n",(num>0) && (num % 2 ==0) );
+
+return 0;
+
+      }
--- exercicios7.cpp
+++ exercicios7.cpp
@@ -8,11 +8,6 @@ int main() {
     printf("Digite um número inteiro: ");
     scanf("%d", &numero);
 
-    // Verifica se o número é negativo
-    int resultado = (numero < 0) ? 1 : 0;
-
-    // Imprime o resultado (1 para negativo, 0 para positivo ou zero)
-    printf("%d\n", resultado);
-
+    printf("%d\n",numero < 0);
     return 0;
 }
